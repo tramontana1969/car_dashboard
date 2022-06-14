4e4eef9f355566d180731bfcff1aac9900ce861a
User <snegovskojm@gmail.com>
2022-06-14 16:14:40 +0300

    delete 'console.log' in script.js

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,6 @@ const select = document.querySelector('select').getElementsByTagName('option');
 const urlParams = new URLSearchParams(window.location.search);
 const param = urlParams.get('year_order');
 
-console.log(select)
 for (let i = 0; i < select.length; i++) {
     if (param === select[i].value) {
         select[i].selected = true;

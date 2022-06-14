48fd0f5a4c1e472bfa98c070020923f42467b877
User <snegovskojm@gmail.com>
2022-06-14 17:06:59 +0300

    optimized Model.php

--- model/Model.php
+++ model/Model.php
@@ -5,16 +5,23 @@ require_once __DIR__ . '/../connection/Connection.php';
 class Model
 {
     public object $connection;
+    public static object $db;
 
     public function __construct()
     {
         $this->connection = new Connection();
+        self::$db = $this->connection->connectToDatabase();
+    }
+
+    private function sendQuery(string $query): object
+    {
+        return self::$db->query($query);
     }
 
     public function getYearSales(): array
     {
         $query = "SELECT AVG(price) as average FROM Showroom_cars WHERE sign = 'sold';";
-        $data = $this->connection->connectToDatabase()->query($query);
+        $data = $this->sendQuery($query);
 
         return $data->fetch();
     }
@@ -23,7 +30,7 @@ class Model
     {
         $today = date("Y-m-d");
         $query = "SELECT AVG(price) as average FROM Showroom_cars WHERE date_of_sale = '$today'; ";
-        $data = $this->connection->connectToDatabase()->query($query);
+        $data = $this->sendQuery($query);
 
         return $data->fetch();
     }
@@ -34,7 +41,7 @@ class Model
         $query = "SELECT date_of_sale as date, COUNT(vehicle_id) as cars 
                   FROM Showroom_cars WHERE date_of_sale IS NOT NULL 
                   AND date_of_sale > '$last_year' GROUP BY date_of_sale;";
-        $data = $this->connection->connectToDatabase()->query($query);
+        $data = $this->sendQuery($query);
 
         return $data->fetchAll();
     }
@@ -45,7 +52,7 @@ class Model
                   Showroom_cars.color as color, Showroom_cars.price as price FROM Vehicle_drectory 
                   JOIN Showroom_cars ON Vehicle_drectory.id = Showroom_cars.vehicle_id
                   WHERE Showroom_cars.sign = 'not sold' ORDER BY $order;";
-        $data = $this->connection->connectToDatabase()->query($query);
+        $data = $this->sendQuery($query);
 
         return $data->fetchAll();
     }
@@ -55,7 +62,7 @@ class Model
         $query = "SELECT model as model, COUNT(model) as quantity 
                   FROM Vehicle_drectory JOIN Showroom_cars ON Vehicle_drectory.id = Showroom_cars.vehicle_id
                   WHERE Showroom_cars.sign = 'not sold' GROUP BY model;";
-        $data = $this->connection->connectToDatabase()->query($query);
+        $data = $this->sendQuery($query);
 
         return $data->fetchAll();
     }
